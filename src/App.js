bd434548de793b219359b0f29ba4877b7ed5af3c
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2021-10-06 11:59:00 +0530

    Setup basic create react app project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-gray-700 min-h-screen">
+      <nav className="w-full px-4 py-6 bg-gray-900 ">
+        <p className="flex items-center text-2xl font-bold">Team 2</p>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
