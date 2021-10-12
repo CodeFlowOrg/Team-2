9245a9f103651ebaab5f35cfb0c7a6f7d7f27181
kanchi12P <78892305+kanchi12P@users.noreply.github.com>
2021-10-12 13:43:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import App from './components/signup.js';
 
 ReactDOM.render(
   < App / >, document.getElementById('root')
 );
+
+
