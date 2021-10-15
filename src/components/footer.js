5b529d9d257c6ce54be6167e8a5dbc725119c8c3
kanchi12P <kanchipardhi@gmail.com>
2021-10-15 16:59:16 +0530

    updated contact form

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,26 @@ import {Grid,Paper} from '@material-ui/core';
 import img1 from '../images/fb.png'
 import img2 from '../images/insta.png'
 import img3 from '../images/twitter.png'
-
+import {
+    Button,
+    TextField,
+    Link,
+    InputLabel,
+    Box,
+    Typography,
+    Container
+  } from "@mui/material";
 function footer(){
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        console.log({
+          email: data.get("email"),
+          message: data.get("message")
+        });
+      };
+  
   return(
       <div style={{fontFamily:"sans-serif", backgroundColor:"#294A63", color:"#a6a6a6",marginTop:"1%"}}>
     <Grid container spacing={2}>
@@ -22,10 +40,46 @@ function footer(){
       </ul>
   </Grid><div>
    <h2>Contact Us</h2><br/>
-    <form><input style={{margi:"1%"}}placeholder="Email ID"/><br/>
-    <textarea style={{margin:"1%"}} placeholder="Message"></textarea>
-    <button style={{margin:"1%", border:"none" ,backgroundColor:"#a6a6a6"}}type="submit">Submit</button>
-        </form>
+   <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+            
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="message"
+                  label="message"
+                  type="message"
+                  id="message"
+                />
+              </Grid>
+              
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+          </Box>
         </div>
   <Grid item xs={12} md={4}>
 
